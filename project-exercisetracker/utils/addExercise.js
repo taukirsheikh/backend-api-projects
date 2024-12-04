ed98e94345e78f854e01dd483a040b71d2b1d59f
taukir <taukir@portpro.io>
2024-12-04 19:38:32 +0545

    filter of from to and limit added if req.query is done

--- project-exercisetracker/utils/addExercise.js
+++ project-exercisetracker/utils/addExercise.js
@@ -1,25 +1,34 @@
 const User = require('../Schema/User');
-
 const addExercise = async (req, res) => {
     try {
         console.log("Adding exercise");
         const { _id } = req.params;
-        const { description, duration, date=new Date().toDateString() } = req.body;
+        const {
+          description,
+          duration,
+          date = new Date().toISOString().split('T')[0], // Default to current date if not provided
+        } = req.body;
+
+
         const user = await User.findById(_id);
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
-        let exercise = { description, duration, date: new Date(date).toDateString() }
+
+        let exercise = { description, duration, date};
         Object.assign(user, exercise);
         user.log.push(exercise);
-        user.count=user.log.length;
-        console.log(user);
+        user.count = user.log.length;
+        
         await user.save();
-        // Optionally exclude 'log' field from the response:
-        const { log,count, ...userWithoutLog } = user.toObject();
+        
+        user.date = new Date(user.date).toDateString();
+        // Optionally exclude 'log' field from the response
+        const { log, count, ...userWithoutLog } = user.toObject();
+        console.log(user);
         res.json(userWithoutLog);
     } catch (error) {
-        res.status(400).json({ error: error });
+        res.status(400).json({ error: error.message });
     }
 };
 
--- project-exercisetracker/utils/getExerciseLogs.js
+++ project-exercisetracker/utils/getExerciseLogs.js
@@ -3,7 +3,15 @@ const User = require('../Schema/User');
 const getExerciseLogs=async(req,res) =>{
     try{
         const {_id}=req.params;
+        // GET /api/users/12345/logs?from=2024-01-01&to=2024-01-31&limit=10
+
+        const { from, to, limit } = req.query;
         const users = await User.findById(_id).select('-description -duration -date');
+        if(from) users.log=users.log.filter(item=>new Date(item.date) >= new Date(from));
+        if(to) users.log=users.log.filter(item=>new Date(item.date) <= new Date(to));
+        if(limit) users.log=users.log.slice(0, limit);
+        // Convert date strings back to dates before returning
+        users.log=users.log.map(item=>item.date=new Date(item.date).toDateString());
         res.json(users);
     }catch(error){
         res.status(400).json({error: error});
